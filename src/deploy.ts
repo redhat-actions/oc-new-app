b613c043bdbbe50cb719c13c42ecec03ea01e691
divyansh42 <diagrawa@redhat.com>
2021-03-09 00:09:05 +0530

    Add input 'namespace' to provide namespace in which to create deployment
    Signed-off-by: divyansh42 <diagrawa@redhat.com>

--- src/deploy.ts
+++ src/deploy.ts
@@ -12,22 +12,30 @@ namespace Deploy {
      * Creates new app with the image provided
      * @param appName Name of the app to use in 'oc new-app' command
      * @param image Image to create application from
+     * @param namespace Namespace in which to create new app
      */
-    export async function newApp(appName: string, image: string): Promise<void> {
+    export async function newApp(appName: string, image: string, namespaceArg: string): Promise<void> {
         ghCore.info("⏳ Creating Deployment from image of the application...");
         const ocOptions = Oc.getOptions({ name: appName, "docker-image": image });
         const ocExecArgs = [ Oc.Commands.NewApp, ...ocOptions ];
+        if (namespaceArg) {
+            ocExecArgs.push(namespaceArg);
+        }
         await Oc.exec(ocExecArgs);
     }
 
     /**
      * Take down any old deployment
-     * @param appSelector selector to filter out deployment
+     * @param appSelector Selector to filter out deployment
+     * @param namespace Namespace from which to delete deployment
      */
-    export async function deleteDeployment(appSelector: string): Promise<void> {
+    export async function deleteDeployment(appSelector: string, namespaceArg: string): Promise<void> {
         ghCore.info("🔍 Checking for old deployments and deleting if found...");
         const ocOptions = Oc.getOptions({ selector: appSelector });
         const ocExecArgs = [ Oc.Commands.Delete, Oc.SubCommands.All, ...ocOptions ];
+        if (namespaceArg) {
+            ocExecArgs.push(namespaceArg);
+        }
         await Oc.exec(ocExecArgs);
     }
 
@@ -35,8 +43,9 @@ namespace Deploy {
      * Patch the created service with the port to expose
      * @param appName Name of the app for which to patch the service
      * @param port Port to expose
+     * @param namespace Namespace where service is created
      */
-    export async function patchSvc(appName: string, port: string): Promise<void> {
+    export async function patchSvc(appName: string, port: string, namespaceArg: string): Promise<void> {
         ghCore.info(`⏳ Patching service with the port ${port} ...`);
         const portInt = Number(port);
         const patchJson = {
@@ -52,44 +61,59 @@ namespace Deploy {
         const patchJsonString = JSON.stringify(patchJson);
         const ocOptions = Oc.getOptions({ patch: patchJsonString });
         const ocExecArgs = [ Oc.Commands.Patch, Oc.SubCommands.Service, appName, ...ocOptions ];
+        if (namespaceArg) {
+            ocExecArgs.push(namespaceArg);
+        }
         await Oc.exec(ocExecArgs);
     }
 
     /**
-     * Expose he route of the application service
+     * Expose the route of the application service
      * @param appName Name of the app for which to expose the route
      * @param svcPort Port to expose
+     * @param namespace Namespace where created app exists
      */
-    export async function exposeSvc(appName: string, svcPort: string): Promise<void> {
+    export async function exposeSvc(appName: string, svcPort: string, namespaceArg: string): Promise<void> {
         ghCore.info(`Exposing the route for ${appName} service...`);
         const ocOptions = Oc.getOptions({ port: svcPort });
         const ocExecArgs = [ Oc.Commands.Expose, Oc.SubCommands.Service, appName, ...ocOptions ];
+        if (namespaceArg) {
+            ocExecArgs.push(namespaceArg);
+        }
         await Oc.exec(ocExecArgs);
     }
 
     /**
      * Get all the deployments based on the selector filter
      * @param appSelector Selector to filter out the deployment
+     * @param namespace Namespace in which to get the deployment
      */
-    export async function getDeployment(appSelector: string): Promise<void> {
+    export async function getDeployment(appSelector: string, namespaceArg: string): Promise<void> {
         ghCore.info("⏳ Verifying if deployment is created successfully...");
         const ocOptions = Oc.getOptions({ selector: appSelector });
         const ocExecArgs = [ Oc.Commands.Get, Oc.SubCommands.All, ...ocOptions ];
+        if (namespaceArg) {
+            ocExecArgs.push(namespaceArg);
+        }
         await Oc.exec(ocExecArgs);
     }
 
     /**
      * Get route of the exposed service of the application
      * @param appName Name of the app for which to find the route
+     * @param namespace Namespace in which created app exists
      */
-    export async function getRoute(appName: string): Promise<string> {
+    export async function getRoute(appName: string, namespaceArg: string): Promise<string> {
         ghCore.info(`⏳ Fetching route of the ${appName} application...`);
-        const jsonPath = "'{.spec.host}'";
-        const ocOptions = Oc.getOptions({ output: "" });
+        const jsonPath = "{.spec.host}";
+        const outputOcOptions = Oc.getOptions({ output: "" });
         const ocExecArgs = [
             // add new line to 'oc get route' command output
-            Oc.Commands.Get, Oc.SubCommands.Route, appName, ...ocOptions, `jsonpath=${jsonPath}{"\\n"}`,
+            Oc.Commands.Get, Oc.SubCommands.Route, appName, ...outputOcOptions, `jsonpath=${jsonPath}{"\\n"}`,
         ];
+        if (namespaceArg) {
+            ocExecArgs.push(namespaceArg);
+        }
         const execResult = await Oc.exec(ocExecArgs);
         return execResult.out;
     }
--- src/index.ts
+++ src/index.ts
@@ -14,23 +14,33 @@ async function run(): Promise<void> {
 
     const appName = ghCore.getInput(Inputs.APP_NAME);
     const image = ghCore.getInput(Inputs.IMAGE);
+    const namespace = ghCore.getInput(Inputs.NAMESPACE);
     const port = ghCore.getInput(Inputs.PORT);
 
     const appSelector = utils.getSelector(appName);
+    let namespaceArg = "";
+    if (namespace) {
+        namespaceArg = `--namespace=${namespace}`;
+    }
+    else {
+        ghCore.info(`No namespace provided`);
+    }
 
     // Take down any old deployment
-    await Deploy.deleteDeployment(appSelector);
+    await Deploy.deleteDeployment(appSelector, namespaceArg);
 
-    await Deploy.newApp(appName, image);
+    await Deploy.newApp(appName, image, namespaceArg);
 
     // Make sure the app port is exposed
-    await Deploy.patchSvc(appName, port);
+    await Deploy.patchSvc(appName, port, namespaceArg);
 
-    await Deploy.exposeSvc(appName, port);
+    await Deploy.exposeSvc(appName, port, namespaceArg);
 
-    await Deploy.getDeployment(appSelector);
+    await Deploy.getDeployment(appSelector, namespaceArg);
 
-    const route = await Deploy.getRoute(appName);
+    let route = await Deploy.getRoute(appName, namespaceArg);
+    // To make it appear as a URL
+    route = `http://${route}`;
     ghCore.info(`✅ ${appName} is exposed at ${route}`);
 
     ghCore.setOutput(Outputs.ROUTE, route);
